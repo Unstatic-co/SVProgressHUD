81cbec9c86c189c88cb2e7aa6b6f56d02cceab99
Richard Hodge <richard.hodge@flueid.com>
2022-09-13 13:03:25 -0500

    Updated to swift 5.7 package

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
   name: "SVProgressHUD",
   platforms: [
-    .iOS(.v9), .tvOS(.v9)
+    .iOS(.v12), .tvOS(.v12)
   ],
   products: [
     .library(name: "SVProgressHUD", targets: ["SVProgressHUD"])
